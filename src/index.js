c2dabe739372e90bbb3042afcf2bd64d4ff90ac5
PaCCiFFisT <pacciffist@gmail.com>
2021-03-03 23:55:28 +0200

    feat: complete solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  if (n < 0) {
+    n = n * (-1);
+    console.log(n);
+  };
+  n = n.toString().split('').reverse().join('');
+  return +n;
 }
